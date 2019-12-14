178b92f12bd4a62ea089579ccf70eba191359c78
Hamza Chebil <hamzachebil40@gmail.com>
2019-12-14 02:20:49 +0100

    voting listing page

--- anteris/src/app/components/vote/vote.component.ts
+++ anteris/src/app/components/vote/vote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VoteService } from 'src/app/services/vote.service';
 
 @Component({
   selector: 'app-vote',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteComponent implements OnInit {
 
-  constructor() { }
+  votes;
+
+  constructor(private voteService: VoteService) { }
 
   ngOnInit() {
+    this.voteService.findAll().subscribe(data => {
+      this.votes = data;
+    })
   }
 
 }
