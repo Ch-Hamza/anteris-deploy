82e750a69c71d06004d2353977d0d97196159f37
Hamza Chebil <hamzachebil40@gmail.com>
2020-01-15 17:05:52 +0100

    Update app-routing.module.ts

--- anteris/src/app/app-routing.module.ts
+++ anteris/src/app/app-routing.module.ts
@@ -8,22 +8,27 @@ import { DonationComponent } from './components/donation-components/donation/don
 import { VoteComponent } from './components/vote-components/vote/vote.component';
 import { VoteFormComponent } from './components/vote-components/vote-form/vote-form.component';
 import {UserListComponent} from './components/user-crud-components/user-list/user-list.component';
+import { ListMembersComponent } from './components/list-members/list-members.component';
+import { EditMemberComponent } from './components/edit-member/edit-member.component';
+import { DonationListComponent } from './components/donation-list/donation-list.component';
+import { VoteEditComponent } from './components/vote-edit/vote-edit.component';
+import { VoteDetailsComponent } from './components/vote-components/vote-details/vote-details.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-
+  { path: 'donation', component: DonationComponent, canActivate: [AuthGuard] },
+  { path: 'donation-list', component: DonationListComponent, canActivate: [AuthGuard] },
+  { path: 'vote', component: VoteComponent, canActivate: [AuthGuard] },
+  { path: 'add-vote', component: VoteFormComponent, canActivate: [AuthGuard] },
+  { path: 'edit-vote/:id', component: VoteEditComponent, canActivate: [AuthGuard] },
+  { path: 'vote-details/:id', component: VoteDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'users-list', component: UserListComponent, canActivate: [AuthGuard] },
   { path: 'member', component: ListMembersComponent, canActivate: [AuthGuard] },
   { path: 'edit-member/:id', component: EditMemberComponent, canActivate: [AuthGuard] },
   { path: 'add-member', component: RegisterComponent, canActivate: [AuthGuard] },
-
-
-  { path: 'donation', component: DonationComponent, canActivate: [AuthGuard] },
-  { path: 'vote', component: VoteComponent, canActivate: [AuthGuard] },
-  { path: 'add-vote', component: VoteFormComponent, canActivate: [AuthGuard] },
-  { path: 'users-list', component: UserListComponent, canActivate: [AuthGuard] },
   // otherwise redirect to home
   { path: '**', redirectTo: 'login' }
 ];
